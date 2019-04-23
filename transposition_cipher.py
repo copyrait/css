3bb1173db35aa1ef7a63ec789602ae9762a5a0c5
copyrait <49820537+copyrait@users.noreply.github.com>
2019-04-23 09:52:45 +0530

    Update transposition_cipher.py

--- transposition_cipher.py
+++ transposition_cipher.py
@@ -1,19 +1,33 @@
+import math
 def split_len(seq, length):
   return [seq[i: i + length]
     for i in range(0, len(seq), length)
   ]
-def encode(key, plaintext):
-    order = {
-        int(val): num
-        for num,
-        val in enumerate(key)
-      }
-    ciphertext = ''
-    
-    for index in sorted(order.keys()):
-      for part in split_len(plaintext, len(key)):
-        try: ciphertext += part[order[index]]
-        except IndexError:
-          continue
-    return ciphertext
-print(encode('3214', 'HELLO'))
+def encode(key, message):
+    ciphertext = [''] * key
+    for col in range(key):
+        position = col
+        while position < len(message):
+            ciphertext[col] += message[position]
+            position += key
+    return ''.join(ciphertext) #Cipher text
+def decryptMessage(key, message):
+   numOfColumns = math.ceil(len(message) / key)
+   numOfRows = key
+   numOfShadedBoxes = (numOfColumns * numOfRows) - len(message)
+   plaintext = [''] * numOfColumns
+   col = 0
+   row = 0
+   
+   for symbol in message:
+      plaintext[col] += symbol
+      col += 1
+      if (col == numOfColumns) or (col == numOfColumns - 1 and row >= numOfRows - numOfShadedBoxes):
+         col = 0;row += 1 
+   return ''.join(plaintext)
+key = 3
+cipher_text = encode(key, 'HELLO')
+plain_text = decryptMessage(key,cipher_text)
+print("Cipher text:", cipher_text)
+print("Plain text:", plain_text)
+
